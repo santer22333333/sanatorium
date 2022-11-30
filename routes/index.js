7b6cc9c19f5b04a738019bb30cc3b1f273d2db16
Sasha <santer.grushko@mail.ru>
2022-11-30 20:13:04 +0300

    q

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/food', function(req, res, next) {
+  res.render('food', { title: 'Express' });
+});
+
+
+
 
 module.exports = router;
